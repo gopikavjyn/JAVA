ea4fe34fbaaa9776c937d13d19cf8a8ddd2545e1
gopikavjyn <94525918+gopikavjyn@users.noreply.github.com>
2022-06-09 16:20:12 +0530

    first commit

--- Rectangle.java
+++ Rectangle.java
@@ -1,42 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package rectangle;
 
-/**
- *
- * @author sjcet
- */
+package graphics;
+
+
 public class Rectangle {
-    int l,b,ar,p;
-    Rectangle()
+    public void area(double l,double b)
     {
-        l=20;
-        b=10;
-        
-    }
-    public void perimeter()
-    {
-        p=2*(l+b);
-        System.out.println("perimeter of rectangle is="+p);
-    }
-    public void area()
-    {
-        ar=l*b;
-        System.out.println("Area of rectangle is="+ar);
-    }
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        {
-            Rectangle r=new Rectangle();
-        r.perimeter();
-        r.area();
-    }
-        // TODO code application logic here
+        System.out.println("Area of rectangle:"+(l*b));
     }
     
 }
